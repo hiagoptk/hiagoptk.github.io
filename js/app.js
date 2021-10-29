6c7725671d4e17413551339163d9ed55598a79ff
hiagoptk <hiogaptk@gmail.com>
2021-10-29 06:00:50 -0300

    atualizando os scripts

--- js/app.js
+++ js/app.js
@@ -1,37 +1,73 @@
+let cont = 0;
 
-// FUNÇÃO QUE FAZ COM QUE O WELCOME DO SITE APAREÇA
-// COMO SE ESTIVE SENDO DIGITADO...
-
-function typeWriter(elemento) {
-    const textoArr = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    textoArr.forEach((letra, i) => {
-        setTimeout(() => {elemento.innerHTML += letra}, 75 * i)
-    });                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+function piscaOn() {
+   if(cont < 1) {
+      document.getElementById("terminal").style.visibility = "hidden";
+      cont++;
+   } else {
+      piscaOff();
+   }
 }
 
+function piscaOff() {
+   cont = 0;
+   document.getElementById("terminal").style.visibility = "visible"
+}
 
-const titulo = document.querySelector('.typed');
-typeWriter(titulo);
+setInterval(piscaOn, 500);
 
-//FUNÇÃO DE ANIMAÇÃO DOS FADE-INS DOS ELEMENTOS DA 
-//PÁGINA ...
 
-const target = document.querySelectorAll('[data-animate]');
+//             ANIMAÇÕES NA PÁGINA
+
+const scrollOffset = 100;
+
+const scrollElement = document.querySelector(".js-scroll");
+console.log("ola");
+
+const elementInView = (el, offset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <= 
+    ((window.innerHeight || document.documentElement.clientHeight) - offset)
+  );
+};
+
+const displayScrollElement = () => {
+  scrollElement.classList.add('.scrolled');
+}
+
+const hideScrollElement = () => {
+  scrollElement.classList.remove('.scrolled');
+}
+
+const handleScrollAnimation = () => {
+  if (elementInView(scrollElement, scrollOffset)) {
+      displayScrollElement();
+  } else {
+    hideScrollElement();
+  }
+}
+
+window.addEventListener('scroll', () => {
+  handleScrollAnimation();
+})
+/*
+const target = document.querySelectorAll('[data-animated]');
 const animationClass = 'animated';
 
 const animeScroll = () => {
-    const windowTop = window.pageYOffset + (window.innerHeight * 0.75);
-
-    target.forEach((item) => {
-        if (windowTop > item.offsetTop){
-            item.classList.add(animationClass);
-        } else {
-            item.classList.remove(animationClass);
-        }
-    })
+   const windowTop = window.pageYOffset + (window.innerHeight * 0.75);
+   
+   target.forEach((item) => {
+      if (windowTop > item.offsetTop) {
+         item.classList.add("animationClass");
+      } else {
+         item.classList.remove("animationClass")
+      }
+   })
 }
 
 animeScroll();
 
-window.addEventListener('scroll', () => { animeScroll() });
+window.addEventListener('scroll', () => { animeScroll() }); */
