a647615ab265460290a3b7baeab3d6f4dce3319f
hiagoptk <hiogaptk@gmail.com>
2021-10-29 23:11:48 -0300

    Finalizando arquivo

--- js/app.js
+++ js/app.js
@@ -19,55 +19,21 @@ setInterval(piscaOn, 500);
 
 //             ANIMAÇÕES NA PÁGINA
 
-const scrollOffset = 100;
-
-const scrollElement = document.querySelector(".js-scroll");
-console.log("ola");
-
-const elementInView = (el, offset = 0) => {
-  const elementTop = el.getBoundingClientRect().top;
-
-  return (
-    elementTop <= 
-    ((window.innerHeight || document.documentElement.clientHeight) - offset)
-  );
-};
-
-const displayScrollElement = () => {
-  scrollElement.classList.add('.scrolled');
-}
-
-const hideScrollElement = () => {
-  scrollElement.classList.remove('.scrolled');
-}
-
-const handleScrollAnimation = () => {
-  if (elementInView(scrollElement, scrollOffset)) {
-      displayScrollElement();
-  } else {
-    hideScrollElement();
-  }
-}
-
-window.addEventListener('scroll', () => {
-  handleScrollAnimation();
-})
-/*
-const target = document.querySelectorAll('[data-animated]');
+const target = document.querySelectorAll('[data-animate]');
 const animationClass = 'animated';
 
 const animeScroll = () => {
-   const windowTop = window.pageYOffset + (window.innerHeight * 0.75);
-   
-   target.forEach((item) => {
-      if (windowTop > item.offsetTop) {
-         item.classList.add("animationClass");
-      } else {
-         item.classList.remove("animationClass")
-      }
-   })
+    const windowTop = window.pageYOffset + (window.innerHeight * 0.75);
+
+    target.forEach((item) => {
+        if (windowTop > item.offsetTop){
+            item.classList.add(animationClass);
+        } else {
+            item.classList.remove(animationClass);
+        }
+    })
 }
 
 animeScroll();
 
-window.addEventListener('scroll', () => { animeScroll() }); */
+window.addEventListener('scroll', () => { animeScroll() });
